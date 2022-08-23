0ff5a06d5714c4a6dd0b44382c370a576e0856ba
Compass <ndphong165@gmail.com>
2022-08-23 23:17:30 +0700

    Su dung Array Index Out Of Bounds Exception

---
+++ Debug/Practice/src/arrayExample/ArrayExample.java
@@ -0,0 +1,34 @@
+package arrayExample;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ArrayExample {
+
+    public static void main(String[] args) {
+        ArrayExample arrayExample = new ArrayExample();
+        Integer[] arr = arrayExample.createRandom();
+
+        Scanner scaner = new Scanner(System.in);
+        System.out.println("\nVui lòng nhập chỉ số của một phần tử bất kỳ: ");
+        int x = scaner.nextInt();
+        try {
+            System.out.println("Giá trị của phần tử có chỉ số " + x + " là " + arr[x]);
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Chỉ số vượt quá giới hạn của mảng");
+        }
+    }
+
+
+    public Integer[] createRandom() {
+        Random rd = new Random();
+        Integer[] arr = new Integer[100];
+        System.out.println("Danh sach phan tu cua mang: ");
+        for (int i = 0; i < 100; i++) {
+            arr[i] = rd.nextInt(100);
+            System.out.print(arr[i] + " ");
+        }
+        return arr;
+    }
+
+}
